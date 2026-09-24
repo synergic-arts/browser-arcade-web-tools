ecf89bb71b3a1a81b9da49509d2bcbefd7fbb626
synergic-arts <synergic-arts@users.noreply.github.com>
2026-09-24 02:59:26 +0200

    feat: add ridge recon exploration game

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ Colección de juegos web gratuitos, estáticos y ejecutables en el navegador.
 - [Nebula Command](juegos/nebula-command/): shooter de oleadas con partículas, escudo, mejoras y comandantes.
 - [Cartographer Quest](juegos/cartographer-quest/): exploración procedural con niebla de guerra, minimapa y seis hitos.
 - [Carto Tactics](juegos/carto-tactics/): estrategia por turnos con terreno, energía, balizas y rival automático.
+- [Ridge Recon](juegos/ridge-recon/): exploración táctica con niebla de guerra, escaneo, anomalías, tormentas y patrullas móviles.
 
 ## Desarrollo
 
--- sw.js
+++ sw.js
@@ -6,7 +6,8 @@ const ASSETS = [
   './juegos/memory-layers/', './juegos/memory-layers/index.html', './juegos/memory-layers/styles.css', './juegos/memory-layers/app.js',
   './juegos/nebula-command/', './juegos/nebula-command/index.html', './juegos/nebula-command/styles.css', './juegos/nebula-command/app.js',
   './juegos/cartographer-quest/', './juegos/cartographer-quest/index.html', './juegos/cartographer-quest/styles.css', './juegos/cartographer-quest/app.js',
-  './juegos/carto-tactics/', './juegos/carto-tactics/index.html', './juegos/carto-tactics/styles.css', './juegos/carto-tactics/app.js'
+  './juegos/carto-tactics/', './juegos/carto-tactics/index.html', './juegos/carto-tactics/styles.css', './juegos/carto-tactics/app.js',
+  './juegos/ridge-recon/', './juegos/ridge-recon/index.html', './juegos/ridge-recon/styles.css', './juegos/ridge-recon/app.js'
 ];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
